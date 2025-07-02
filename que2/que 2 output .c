5d2c22dacfb5bcda66abf3ab164a37779abc6e7e
Aashishshaw <rajaashishkumar2005@gmail.com>
2025-07-02 13:26:13 +0530

    que2 output .c
    sample output

---
+++ que2/que 2 output .c
@@ -0,0 +1,54 @@
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  # sample output :
+
+  Lexical Analysis Output:
+
+[Keyword]     : int
+[Identifier]  : main
+[Special Char]: (
+[Special Char]: )
+[Special Char]: {
+[Keyword]     : int
+[Identifier]  : x
+[Operator]    : =
+[Number]      : 10
+[Special Char]: ;
+[Keyword]     : float
+[Identifier]  : y
+[Operator]    : =
+[Identifier]  : x
+[Operator]    : +
+[Number]      : 3
+[Special Char]: .
+[Number]      : 5
+[Special Char]: ;
+[Keyword]     : if
+[Special Char]: (
+[Identifier]  : x
+[Operator]    : <
+[Identifier]  : y
+[Special Char]: )
+[Special Char]: {
+[Identifier]  : x
+[Operator]    : =
+[Identifier]  : x
+[Operator]    : +
+[Number]      : 1
+[Special Char]: ;
+[Special Char]: }
+[Keyword]     : else
+[Special Char]: {
+[Identifier]  : y
+[Operator]    : =
+[Identifier]  : y
+[Operator]    : -
+[Number]      : 1
+[Special Char]: .
+[Number]      : 0
+[Special Char]: ;
+[Special Char]: }
+[Keyword]     : return
+[Number]      : 0
+[Special Char]: ;
+[Special Char]: }
